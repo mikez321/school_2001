0e7b21775603af3ab8adb982669af9d5e6831f0e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-29 13:08:06 -0700

    Add attr_reader for start time

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,7 @@
 class School
-  def initialize(time, students)
-    @time = time
+  attr_reader :start_time
+  def initialize(start_time_parameter, students)
+    @start_time = start_time_parameter
     @students = students
   end
 end

cd838aed9bc7b4adcfb66b78b5a45111a256ea4a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-29 13:29:12 -0700

    Make end time method

--- lib/school.rb
+++ lib/school.rb
@@ -5,4 +5,18 @@ class School
     @hours_in_school_day = hours_in_school_day_parameter
     @student_names = []
   end
+
+  def add_student_name(student_name)
+    @student_names << student_name
+  end
+
+  def end_time
+    time_ary = start_time.split("")
+    new_time = time_ary[0].to_i + hours_in_school_day.to_i
+    def make_time(hours)
+      "#{hours}:00"
+    end
+    make_time(new_time)
+  end
+
 end

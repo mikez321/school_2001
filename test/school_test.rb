1e45582127f7862328886877ffbba02250550ba6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-29 13:57:57 -0700

    Create but skip standardized names test

--- test/school_test.rb
+++ test/school_test.rb
@@ -5,24 +5,28 @@ require './lib/school'
 class SchoolTest < Minitest::Test
   #Iteration 1 Tests:
   def test_it_exists
+    skip
     school = School.new('9:00', 7)
 
     assert_instance_of School, school
   end
 
   def test_it_has_start_time
+    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
+    skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
+    skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -30,6 +34,7 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
+    skip
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -40,6 +45,7 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_can_calculate_end_time
+    skip
     school1 = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
 
@@ -48,11 +54,24 @@ class SchoolTest < Minitest::Test
   end
 
   def test_school_is_full_time
+    skip
     school = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
 
     assert_equal true, school.full_time
     assert_equal false, school2.full_time
-
   end
+
+  def test_standardize_student_names_with_capital_letters
+    skip
+    school = School.new('9:00', 7)
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    assert_equal ['Aurora', 'tim', 'megan'], school.student_names
+    school.standard_student_names
+    assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
+  end
+
 end
